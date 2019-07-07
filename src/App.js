cb43a023788fb70ba16eb39fb7fcb674d49741ac
Jocelyn <791245613@qq.com>
2019-07-07 16:20:21 +0800

    初始化完成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 根
+    <Fragment>
+<label htmlFor="myinput">银行卡密码:</label>
+<input id="myinput" placeholder="请输入银行卡密码" />
+    </Fragment>
   );
 }
 
